d57a7a12769818c73a8094dfe5c1b3579154b997
AlisaZil <alisazilberman@gmail.com>
2022-08-17 21:50:50 -0700

    add(service-page component, main-page component and generic card component)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ServicesPageComponent } from './services-page/services-page.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    ServicesPageComponent,
+    MainPageComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule
