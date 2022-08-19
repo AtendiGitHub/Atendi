d6a2b25b7a9218582a73c57c2aa51056072cdf4f
AlisaZil <alisazilberman@gmail.com>
2022-08-19 12:50:46 -0700

    move(button and card components to generic element folder)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,9 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { ServicesPageComponent } from './services-page/services-page.component';
 import { MainPageComponent } from './main-page/main-page.component';
-import { CardComponent } from './card/card.component';
+import { CardComponent } from './generic-elements/card/card.component';
 import { MainSectionComponent } from './main-page/main-section/main-section.component';
-import { ButtonComponent } from './button/button.component';
+import { ButtonComponent } from './generic-elements/button/button.component';
 
 @NgModule({
   declarations: [
