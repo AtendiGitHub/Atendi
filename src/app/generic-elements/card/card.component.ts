8d0ef2973d5d19098eab68b41313f8d21e5459d1
AlisaZil <alisazilberman@gmail.com>
2022-08-19 18:42:53 -0700

    add(changeble css variables)

--- src/app/generic-elements/card/card.component.ts
+++ src/app/generic-elements/card/card.component.ts
@@ -7,15 +7,23 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
-  @Input() cssVariables:{height:string, width:string, backgroundColor:string} = {height:"", width:"", backgroundColor:""};
+  @Input() cssVariables:{height:string, width:string, backgroundColor:string, borderRadius:string} =
+  {height:"", width:"", backgroundColor:"", borderRadius:""};
+  
   @Input() iconPath:string = "";
   @Input() title:string = "";
   @Input() body:string = "";
 
+  cardCssVariables:any;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.cardCssVariables = document.querySelector(".card-body");
+    this.cardCssVariables.style.height = this.cssVariables.height;
+    this.cardCssVariables.style.width = this.cssVariables.width;
+    this.cardCssVariables.style.background = this.cssVariables.backgroundColor;
+    this.cardCssVariables.style.borderRadius = this.cssVariables.borderRadius;
   }
 
 }
