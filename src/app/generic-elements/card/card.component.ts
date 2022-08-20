0edd0fe330b9c2eb29d86a30d48d253cea4ca7ce
AlisaZil <alisazilberman@gmail.com>
2022-08-19 22:36:41 -0700

    add(cards data for main section and overall small changes)

--- src/app/generic-elements/card/card.component.ts
+++ src/app/generic-elements/card/card.component.ts
@@ -14,8 +14,6 @@ export class CardComponent implements OnInit{
   @Input() title:string = "";
   @Input() body:string = "";
 
-  cardCssVariables:any;
-
   ngOnInit(): void {
   }
 }
--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPageComponent implements OnInit {
 
-  cardCssVariables:{height:string, width:string, backgroundColor:string, borderRadius:string} = {height:'200px', width:'200px', backgroundColor:'white', borderRadius:'0px'};
-
-  constructor() { }
+  cardCssVariables:{height:string, width:string, backgroundColor:string, borderRadius:string} = 
+  {height:'200px', width:'200px', backgroundColor:'pink', borderRadius:'0px'};
+  
+  cardsData:{iconPath:string, title:string, body:string}[] = [
+    {
+      iconPath:"",
+      title:"Flexibility & Resources",
+      body:"create and manage a classroom or a conference room with total flexibility and resources rarely available in a physical setting from any device with an internet connection"
+    },
+    {
+      iconPath:"",
+      title:"Supports Large Audiences",
+      body:"A classroom or a conference room of infinite size, where every student or participant is in the first row. An infinite classroom or a conference room, where the physical location of the participants is unrestricted."
+    }
+  ]
 
   ngOnInit(): void {
   }
