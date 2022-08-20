018f328988ee4fb54557fbe636fda98bac14377b
AlisaZil <alisazilberman@gmail.com>
2022-08-19 18:13:08 -0700

    add(honda less var to js package)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { ServicesPageComponent } from './services-page/services-page.component';
