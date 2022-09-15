d94fb4503b56f107672854a837641545cf3efbe0
AlisaZil <alisazilberman@gmail.com>
2022-09-14 17:54:36 -0700

    change(tooltip in contant page to tooltip componrnt)

--- src/app/generic-components/tooltip/tooltip.component.ts
+++ src/app/generic-components/tooltip/tooltip.component.ts
@@ -1,16 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-tooltip',
   templateUrl: './tooltip.component.html',
   styleUrls: ['./tooltip.component.less']
 })
-export class TooltipComponent implements OnInit {
+export class TooltipComponent {
 
   @Input() context:string = "help!";
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
